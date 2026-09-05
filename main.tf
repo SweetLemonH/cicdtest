9881bc85998045dc12b4cd8497d84b9e674070d4
HuangMeiting <1904224869@qq.com>
2026-09-05 14:12:14 +0900

    Initial Terraform CI/CD setup

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "cicdtest-vpc"
+  }
+}
